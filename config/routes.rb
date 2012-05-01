6abe114fc8b58e7c1ae85ed5cbdb2d2adc353a5a
Ryan Krueger <ryankrueger@gmail.com>
2012-04-30 21:48:06 -0700

    finish layout and routes 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
+  match '/signup',  to: 'users#new'
+
   match '/help', to: 'static_pages#help'
 
   match '/about', to: 'static_pages#about'
