7dfa5557c0a6d56dae6f94f6dd9ac248aeb7122c
Ryan Krueger <ryankrueger@gmail.com>
2012-05-04 22:28:39 -0700

    half way through sign-up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,12 @@ SampleApp::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  # Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
+
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
