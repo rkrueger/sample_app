f1b4eec9436b776bff75d484ff5bdef79c3d3543
Ryan Krueger <ryankrueger@gmail.com>
2012-05-06 23:49:09 -0700

    Finish sign in - still have some rspec errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,7 @@ describe User do
     it { should respond_to(:password_digest) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
 	it {should respond_to(:authenticate) }
 
 	it { should be_valid }
@@ -110,4 +111,9 @@ end
 
     it { should_not be_valid }
 	end
+
+  describe "remember_token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
